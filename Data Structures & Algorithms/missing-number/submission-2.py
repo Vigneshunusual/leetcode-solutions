bd56cdd278d72d43fdf85f03718e0c28b29cf3d2
Vignesh R <133629748+Vigneshunusual@users.noreply.github.com>
2026-06-05 20:19:37 +0530

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        # n=len(nums)
+        # e_sum=n*(n+1)//2   
+        # a_sum=0            
+        # for i in nums:
+        #     a_sum+=i
+        # return e_sum-a_sum
+
+
+        res=len(nums)
+        for i in range(len(nums)):
+            res=res^i^nums[i]
+        return res
+        
